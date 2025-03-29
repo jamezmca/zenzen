b074cf4aaef2b006a1e9f9729490a9aefa534618
James McArthur <77163783+jamezmca@users.noreply.github.com>
2025-03-29 21:15:14 +1300

    edit name

--- src/components/Stats.jsx
+++ src/components/Stats.jsx
@@ -10,14 +10,17 @@ export default function Stats(props) {
 
   const completedLength = data?.[currDateString] ? Object.keys(getCompletedData(data?.[currDateString])).length : 0
 
-  console.log(data)
-
   return (
     <>
       <div className='stats-card'>
         <div className='stats-columns'>
           <div className='welcome-text card double-col'>
-            <p>Welcome</p>
+            <div className='edit-name'>
+              <p>Welcome</p>
+              <button onClick={() => handleChangePage(0)} className='card-button-secondary'>
+                <i className="fa-solid fa-pen-to-square"></i>
+              </button>
+            </div>
             <h4 className='text-medium'>
               {name}
             </h4>
